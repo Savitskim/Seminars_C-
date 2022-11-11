867d5261828290db2b18f93ef73ed5e840656491
Savitskim <savitskim@inbox.ru>
2022-11-11 21:33:12 +0300

    Task2,Task3

---
+++ Task2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите два числа: ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if (a * a == b)
+{
+    Console.WriteLine("Является");
+}
+else
+{
+    Console.WriteLine("Не является");
+}
+
---
+++ Task3/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("Введите число: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day == 1)
+{
+    Console.WriteLine("1.Понедельник");
+}
+else if (day == 2)
+{
+    Console.WriteLine("2.Вторник");
+}
+else if (day == 3)
+{
+    Console.WriteLine("3.Среда");
+}
+else if (day == 4)
+{
+    Console.WriteLine("4.Четверг");
+}
+else if (day == 5)
+{
+    Console.WriteLine("5.Пятница");
+}
+else if (day == 6)
+{
+    Console.WriteLine("6.Суббота");
+}
+else if (day == 7)
+{
+    Console.WriteLine("7.Воскресенье");
+}
+else
+{
+    Console.WriteLine("Число внедиапазона");
+}
