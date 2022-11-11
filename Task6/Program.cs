c4ae27af19d718a0148c43e116ef2f97e223c3ac
Savitskim <savitskim@inbox.ru>
2022-11-11 22:04:29 +0300

    Task4,Task5,Task6

---
+++ Task6/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите трехзначное число чтобы узнать третью цифру: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 1000 && number > 99)
+{
+    number = number % 10;
+
+    Console.WriteLine($"Третья цифра {number}");
+}
+else
+{
+    Console.WriteLine("Трехзначное число *******");
+}
