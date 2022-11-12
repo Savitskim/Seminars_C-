7e173ba6d5fc9804376098c5f53fdd74d31eabb1
Savitskim <savitskim@inbox.ru>
2022-11-12 09:23:31 +0300

    Correct Home Work3

--- HomeTask6/Program.cs
+++ HomeTask6/Program.cs
@@ -5,4 +5,4 @@
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
 if (number % 2 == 0) Console.WriteLine("является четным");
-else Console.WriteLine("является нечетным");
+else Console.WriteLine("не является четным");
