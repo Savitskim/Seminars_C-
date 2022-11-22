303d40372460a18153aaebc2020c490a84df08b6
Savitskim <savitskim@inbox.ru>
2022-11-23 01:13:55 +0300

    HomeWork4

--- HelpCode)/Program.cs
+++ HelpCode)/Program.cs
@@ -1,44 +1,67 @@
-﻿void PrintArray(int[] array)
+﻿// Проверяет массив на палиндром
+
+void PrintArray(int[] array)
 {
     int count = array.Length;
     for (int i = 0; i < count; i++)
     {
-        Console.Write($"{ array[i]}");
+        Console.Write($"{array[i]} ");
     }
-    Console.WriteLine();
 }
-// void VS(int[] massive, int[] revers)
+
+void VS(int[] massive)
+{
+    int i = 0;
+    int yes = 0;
+    int size = massive.Length - 1;
+    while (i < massive.Length)
+    {
+        if (massive[i] == massive[size])
+        {
+            yes = yes + 1;
+        }
+        else break;
+        i++;
+        size--;
+    }
+    if (yes == massive.Length) Console.WriteLine(" -> Да");
+    else Console.WriteLine(" -> Нет");
+}
+
+
+void FillArray(int[] arr)
+{
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"Введите элемент массива под индексом {i}: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+Console.WriteLine("Введите кол-во элементов массива: ");
+int count = Math.Abs(Convert.ToInt32(Console.ReadLine()));
+int[] myArray = new int[count];
+FillArray(myArray);
+PrintArray(myArray);
+VS(myArray);
+
+// int[] ReversArray(int[] massive)
 // {
-//     int yes=0;
-//     for(int index=0;index<revers.Length;index++)
+//     int size = 4;
+//     int i = 0;
+//     while (i < size)
 //     {
-//         if(massive[index] == revers[index])
-//         {
-//             yes=yes+1;
-//         }
-//     }
-//     if(yes==revers.Length) Console.WriteLine("Да");
-//     else Console.WriteLine("Нет");
-//     Console.WriteLine(yes);
-// }
-// void ReversArray(int[] massive)
-// {
-//     int size=massive.Length-1;
-//     int i=0;
-//     while(i>size)
-//     {
-//         int temp=massive[i];
-//         massive[i]=massive[size];
-//         massive[size]=temp;
-//         size=size-1;
+//         int temp = massive[i];
+//         massive[i] = massive[size];
+//         massive[size] = temp;
+//         size--;
 //         i++;
 //     }
+//     return massive;
 // }
-// int[] myArray2 = new int[5];
-// int[] myArray = new int[5];
-// for (int i = 0; i<myArray.Length;i++)
-// {
-//     Console.Write($"Введите элемент массива под индексом {i}: ");
-//     myArray[i] = Convert.ToInt32(Console.ReadLine());
-//     myArray2[i]=myArray[i];
-// }
+
+
+
+
+
+
---
+++ HomeTask25/Program.cs
@@ -0,0 +1,38 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа 
+// (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243
+// 2, 4 -> 16
+
+// Через встроенный функционал
+double Degree(double n1, double n2)
+{
+    return Math.Pow(n1, n2);
+}
+
+Console.WriteLine("Введите число А:");
+double a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В (натуральное):");
+double b = Convert.ToInt32(Console.ReadLine());
+if (b < 0) Console.WriteLine("Число В не является натуральным");
+else Console.WriteLine($"{a}, {b} -> {Degree(a, b)}");
+
+
+// Через цикл
+Console.WriteLine("Введите число А:");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число В (натуральное):");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int num1, int num2)
+{
+    int count = 1;
+    int result = num1;
+    while (count < num2)
+    {
+        result = result * num1;
+        count++;
+    }
+    return result;
+}
+if (b < 0) Console.WriteLine("Число В не является натуральным");
+else Console.WriteLine($"{a}, {b} -> {Degree(a, b)}");
